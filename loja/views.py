c7d0604be93dd7e6c076977d897ff9ab44a18061
Arthur Lisboa <127349787+ArthurLBSilva@users.noreply.github.com>
2025-01-14 00:54:25 -0300

    Foi implementada o cadastro de produto na tela inicial
    Agora, ao preencher os campos de nome, preço e quantidade de estoque e clicar em "cadastrar produto", é registrado no banco de dados, a funcionalidade permite continuar cadastrando novos produtos sem a necessidade de navegar para outra página, pois a pagina é renderizada ao clicar no botão junto com um aviso de sucesso, porém é necessário melhorar esse aviso.

--- loja/views.py
+++ loja/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produto
 
 def home(request):
+    if request.method == 'POST':
+        # Verifica se o botão de Cadastrar Produto foi clicado
+        if 'cadastrar_produto' in request.POST:
+            novo_produto = Produto()
+
+            # Atribuindo os valores obtidos do formulário
+            novo_produto.nome = request.POST.get('nome_produto')
+            novo_produto.preco = request.POST.get('preco_produto')
+            novo_produto.quantidade_estoque = request.POST.get('qtd_estoque')
+
+            # Salvando o novo produto no banco de dados
+            novo_produto.save()
+            return render(request, 'produtos/home.html', {'message': 'Produto cadastrado com sucesso!'})
+
+    # Se não for um POST, renderiza o formulário vazio
     return render(request, 'produtos/home.html')
 
 def listagem(request):
-    novo_produto = Produto()
-    nome = request.POST.get()
-    
+    # Obtém todos os produtos cadastrados no banco de dados
+    produtos = Produto.objects.all()
+
+    # Renderiza a página de listagem e passa os produtos como contexto
+    return render(request, 'produtos/listagem.html', {'produtos': produtos})
