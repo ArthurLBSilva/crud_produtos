12689fcc3735fd1ee9701f45f5b866bb3bf82518
Arthur Lisboa <127349787+ArthurLBSilva@users.noreply.github.com>
2025-01-14 21:17:58 -0300

    Organização de codigo
    Organizei mais o codigo junto com mais comentários, além disso criei um arquivo para a estilização da pagina inicial, acredito que a primeira pagina está completa.

--- crud_produtos/settings.py
+++ crud_produtos/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# Define o diretório onde o Django vai procuar os arquivos estaticos
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- loja/models.py
+++ loja/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
-# Validações dos campos e criação da tabela
+# Criação da tabela
 class Produto(models.Model):
     id_produto = models.AutoField(primary_key=True)  # Definindo id_produto como chave primária
-    #Mesmo tendo esse maximo ele não aponta erro na hora de salvar no banco, colocar mais validação no html
+    #Mesmo tendo esse maximo ele não aponta erro na hora de salvar no banco
     nome = models.CharField(max_length=100)
     # O preço tem que ser minimamente maior que 0.01
     preco = models.DecimalField(max_digits=15, decimal_places=2, validators=[MinValueValidator(0.01)])
+    # O minimo do estoque é 0, menos que isso não pode
     quantidade_estoque = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+    # É salvo a data automaticamente ao ser criado
     data_criacao = models.DateTimeField(auto_now_add=True) 
