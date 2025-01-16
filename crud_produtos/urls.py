3d39821c8953a9822b6c7bec4057aff752ea9403
Arthur Lisboa <127349787+ArthurLBSilva@users.noreply.github.com>
2025-01-15 22:22:27 -0300

    Organizei certos detalhes do codigo
    Adicionei mais comentários para ficar de mais facil compreensão.

--- crud_produtos/urls.py
+++ crud_produtos/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from loja import views
+
 urlpatterns = [
-    path('', views.home, name='home'), # Cadastro de produtos, tela inicial
-    path('listagem/', views.listagem, name='listagem_produtos'), # Listagem de produtos
-    path('produto/excluir/<int:id>/', views.excluir_produto, name='excluir_produto'), # Excluir produto
-    path('editar_produto/<int:id>//', views.editar_produto, name='editar_produto'), # Atualizar produto
+    # Rota para a página inicial, onde é possível cadastrar novos produtos
+    path('', views.home, name='home'),
+
+    # Rota para exibir a lista de produtos cadastrados
+    path('listagem/', views.listagem, name='listagem_produtos'),
+
+    # Rota para excluir um produto com base no ID fornecido
+    path('produto/excluir/<int:id>/', views.excluir_produto, name='excluir_produto'),
+
+    # Rota para editar as informações de um produto específico com base no ID fornecido
+    path('editar_produto/<int:id>/', views.editar_produto, name='editar_produto'),
 ]
